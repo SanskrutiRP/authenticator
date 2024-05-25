03c755667d3bf77114f128214e6f9aa091f69b94
Sanskriti Punde <67673060+SanskrutiRP@users.noreply.github.com>
2024-05-26 02:26:44 +0530

    update:auth

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
-import { Controller, Post, Body, Req } from '@nestjs/common';
+import { Controller, Post, Body, Req, Res, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from 'src/user/create-user.dto';
+import { Response } from 'express';
 
 @ApiTags('Auth')
 @Controller('auth')
@@ -9,8 +11,21 @@ export class AuthController {
 
   @ApiOperation({ summary: 'Login user' })
   @Post('login')
-  async login(@Req() req, @Body() payload) {
+  @ApiBody({
+    type: CreateUserDto,
+    description: 'Login user',
+  })
+  @ApiBearerAuth('access-token')
+  async login(
+    @Req() req,
+    @Body() payload: CreateUserDto,
+    @Res() res: Response,
+  ) {
     const token = req.headers.authorization.split(' ')[1];
-    return this.authService.checkUser(token, payload);
+    const response = await this.authService.checkUser(token, payload);
+    if (response && response.data) {
+      return res.status(HttpStatus.OK).send(response);
+    }
+    return res.status(HttpStatus.BAD_REQUEST).send(response);
   }
 }

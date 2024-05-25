20ced7e1ff8544dabf24e123cea2fb0fa89f8480
Sanskriti Punde <67673060+SanskrutiRP@users.noreply.github.com>
2024-05-26 02:29:26 +0530

    update:dto

--- src/user/update-user.dto.ts
+++ src/user/update-user.dto.ts
@@ -1,14 +1,37 @@
-import { IsString, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, IsOptional, Matches, IsUrl } from 'class-validator';
 
 export class UpdateUserDto {
+  @ApiProperty({
+    type: 'string',
+    description: 'Bio',
+  })
   @IsOptional()
   @IsString()
   bio?: string;
 
+  @ApiProperty({
+    type: 'string',
+    description: 'Base 64',
+  })
   @IsOptional()
-  @IsString()
-  profilePhoto?: string;
+  @Matches(/^data:image\/[a-z]+;base64,[A-Za-z0-9+/=]+$/, {
+    message: 'Invalid base64 format',
+  })
+  base64?: string;
 
+  @ApiProperty({
+    type: 'string',
+    description: 'Photo URL',
+  })
+  @IsOptional()
+  @IsUrl()
+  url?: string;
+
+  @ApiProperty({
+    type: 'string',
+    description: 'Profile type',
+  })
   @IsOptional()
   @IsString()
   profileType?: string;
